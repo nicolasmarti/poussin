185ae6aed2cfbbc317027b14a57015d372e4e278
Nicolas Marti <nicolas@nicolas-XPS-8300.(none)>
2012-09-07 08:56:53 +0900

    better name for context vars in interactive

--- src/core/interactive/interactive.ml
+++ src/core/interactive/interactive.ml
@@ -26,8 +26,13 @@ end pb
 let init_interactive =
   (* we add the interactive oracle at the end of the list *)
   oracles := !oracles @ (fun defs ctxt ty -> 
+    (* we just modify the frame of the context, in order to have all bounded variables accessible by a fresh name *)
+    let _, frames = List.fold_right (fun ({name = n; ty = ty } as frame) (ln, acc)  ->
+      let n' = fresh_name_list ~basename:(if String.compare n "" == 0 then "H" else n) ln in
+      (n'::ln), {frame with name = n'}::acc
+    ) ctxt.bvs ([], []) in
     (* build a ref of the context *)
-    let ctxt' = ref ctxt in
+    let ctxt' = ref { ctxt with bvs = frames } in
     (* as the substitution is lazy, we force the possible substitution on the goal type for it to be readable *)
     let s, f = conversion_hyps2subst ~dec_order:true ctxt.conversion_hyps in
     let s = append_substitution s (context2subst ctxt') in
@@ -35,10 +40,6 @@ let init_interactive =
     printf "------------------------------------------\n";
     ignore(map_nth (fun i -> 
       let i' = i - 1 in
-      let n = (bvar_name ctxt' i') in
-      if String.compare n "_" = 0 then
-	()
-      else
 	printf "%s: %s\n" (bvar_name ctxt' i') (term2string ctxt' (term_substitution s (bvar_type ctxt' i')))
   ) (List.length !ctxt'.bvs));
     printf "------------------------------------------\n";
