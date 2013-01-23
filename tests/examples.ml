894244b2594481277f216b98b095942a435cce56
nicolas marti <nicolas.marti@nc-sec.co.jp>
2013-01-23 15:43:46 +0900

    updates

--- tests/examples.ml
+++ tests/examples.ml
@@ -1,8 +1,6 @@
 <:def< Inductive Peano : Set := | O : Peano | S: Peano -> Peano >>;;
-<:def< Signature plus_Peano: Peano -> Peano -> Peano >>;;
-<:def< Decreasing plus_Peano [0] >>;;
 <:def< 
-  Definition plus_Peano (x y: Peano) :=
+  Recursive plus_Peano (x y: Peano) [0] : Peano :=
     match x with
       | O := y
       | S x := S (plus_Peano x y)
