3348ed0bf53d6962d73ef9f549112dec1a4296cd
U-nicolas-PC\nicolas <nicolas@nicolas-PC.(none)>
2014-02-08 20:40:50 +0900

    update tests

--- tests/examples2.ml
+++ tests/examples2.ml
@@ -1,17 +1,17 @@
 let zero = <:term< O >>;;
 
-let caca = match <:term< O >> with 
+let caca = match to_ground_term <:term< O >> with 
   | { ast = Cste "O"; _} -> true 
   | { ast = Lambda ((x, _,Explicit), { ast = TName x0; _}); _} when String.compare x x0 = 0 -> false ;;
 
 
-let prout = match <:term< \\ {A: Set} (y: A) -> y >> with 
-  | <:term< O >> -> true 
-  | <:term< \\ {x} -> y >> -> false
+let prout = match to_ground_term <:term< \\ {A: Set} (y: A) -> y >> with 
+  | <:pat< O >> -> true 
+  | <:pat< \\ {x} -> y >> -> false
 ;;
 
-let popo = match get_type <:term< \\ {A: Set} (y: A) -> y >> with 
-  | <:term< O >> -> true 
-  | <:term<  {x} -> y >> -> false
+let popo = match get_type (to_ground_term <:term< \\ {A: Set} (y: A) -> y >>) with 
+  | <:pat< O >> -> true 
+  | <:pat<  {x} -> y >> -> false
 ;;
 
